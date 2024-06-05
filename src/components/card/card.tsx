02c574dbe7c7baba90e0114cac0d18d66ee30aaa
JaxV9 <layanj.pro@gmail.com>
2024-06-05 17:08:04 +0200

    feat: filter added

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,15 +4,16 @@ type CardPropsType = {
     usersProps: UserType[]
 }
 
-export const Card = ({usersProps}: CardPropsType) => {
+export const Card = ({ usersProps }: CardPropsType) => {
 
     return (
         <>
             {usersProps.length > 0 ?
                 usersProps.map((user, index) => (
                     <div className="card" key={index}>
+                        <div className="deleteCardBtn"></div>
                         <img className="profilImage" src={user.picture.medium} alt="" />
-                        <p>{user.name.title}{user.name.first}</p>
+                        <p>{user.name.title} {user.name.first}</p>
                     </div>
                 ))
                 : null}
--- src/components/cardsList/cardsList.tsx
+++ src/components/cardsList/cardsList.tsx
@@ -10,30 +10,44 @@ import { Button } from "../ui/button/button";
 export const CardsList = () => {
 
     const [users, setUsers] = useState<UserType[]>([])
-    const [userLimit, setUserLimit] = useState<number>(100)
+    const [userLimit, setUserLimit] = useState<number>(10)
     const [currentPage, setCurrentPage] = useState<number>(1)
+    const [currentFilter, setCurrentFilter] = useState<string>("")
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    const fetchUsers = async () => {
+    const fetchUsers = async (params?: string) => {
         try {
             setIsLoading(true)
-            let response = await fetch(`https://randomuser.me/api/?results=${userLimit}&page=${currentPage}`, { cache: 'force-cache' })
+            let response = await fetch(`https://randomuser.me/api/?results=${userLimit}&page=${currentPage}&gender=${currentFilter}`, { cache: 'force-cache' })
             let data = await response.json()
 
-            if(currentPage === 1) {
-                setUsers(data.results)
-            }
-            if(currentPage > 1) {
-                setUsers(prevList => [...prevList, ...data.results])
-            }
+            setUsers(prevList => currentPage === 1 ? data.results : [...prevList, ...data.results]);
 
         } catch {
             console.log("error")
         }
     }
 
+    const paramsFetchUser = async(e: React.ChangeEvent<HTMLSelectElement>) => {
+
+        if(e.target.value === ""){
+            setCurrentFilter("")
+        }
+        if(e.target.value === "male"){
+            setCurrentFilter("male")
+        }
+        if(e.target.value === "female"){
+            setCurrentFilter("female")
+        }
+    }
+
     const fetchMoreUsers = () => {
-        setCurrentPage(currentPage+1)
+        setCurrentPage(currentPage + 1)
+    }
+
+    const handleChangeParams = async () => {
+        setUsers([])
+        fetchUsers()
     }
 
     useEffect(() => {
@@ -42,22 +56,43 @@ export const CardsList = () => {
 
     useEffect(() => {
         fetchUsers()
-    },[currentPage])
+    }, [currentPage])
 
     useEffect(() => {
         setIsLoading(false)
-    },[users])
+    }, [users])
+
+    useEffect(() => {
+        handleChangeParams()
+    },[currentFilter])
 
 
     return (
         <>
             <section className="cardsListSection">
+                <div className="paramsListContainer">
+                    <div className="paramsList">
+                        <label>Filter by gender</label>
+                        <select onChange={(e) => paramsFetchUser(e)}>
+                            <option value="">All</option>
+                            <option value="male">Men</option>
+                            <option value="female">Women</option>
+                        </select>
+                    </div>
+                    <div className="paramsList">
+                        <label>Order by date of birth</label>
+                        <select>
+                            <option value="increase">Increase</option>
+                            <option value="descrease">Decrease</option>
+                        </select>
+                    </div>
+                </div>
                 <span>{users.length} users</span>
                 <div className="cardsListContainer">
                     <Card usersProps={users} />
                 </div>
                 <span>{users.length} users</span>
-                <Button functionProps={fetchMoreUsers} textProps="Load more" isLoadingProps={isLoading}/>
+                <Button functionProps={fetchMoreUsers} textProps="Load more" isLoadingProps={isLoading} />
             </section>
         </>
     )
