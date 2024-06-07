f7d26c7e4d7d0d16fed76c2c8b80c6b0b1dd1f72
JaxV9 <layanj.pro@gmail.com>
2024-06-07 16:06:16 +0200

    feat: cards icons added

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -14,8 +14,15 @@ export const Card = ({ usersProps, deletefuncProps }: CardPropsType) => {
                     <div className="card" key={index}>
                         <div className="deleteCardBtn" onClick={() => deletefuncProps(index)}></div>
                         <img className="profilImage" src={user.picture.medium} alt="" />
-                        <p>{user.name.title} {user.name.first}</p>
-                        <p>{user.dob.age}</p>
+                        {/* <p>{user.name.title} {user.name.first}</p>
+                        <p>{user.dob.age}</p> */}
+                        <div className="navCardContainer">
+                            <div className="userNameIcon"></div>
+                            <div className="userMailIcon"></div>
+                            <div className="userDobIcon"></div>
+                            <div className="userMapIcon"></div>
+                            <div className="userPhoneIcon"></div>
+                        </div>
                     </div>
                 ))
                 : null}
